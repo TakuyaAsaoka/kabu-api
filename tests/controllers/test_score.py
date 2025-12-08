d339cf9b7747b77f04b095cd3868c7941c222003
Asaoka Takuya <dimas080201@gmail.com>
2025-12-08 22:35:32 +0900

    リファクタリング

--- tests/controllers/test_score.py
+++ tests/controllers/test_score.py
@@ -2,22 +2,37 @@ import pytest
 from fastapi.testclient import TestClient
 from unittest.mock import patch
 from app.main import app
+from app.schemas.score import ScoreResponse, MarketCondition, NikkeiMomentum
 
 client = TestClient(app)
 
-@patch("app.controllers.score.get_price_data")
-@patch("app.controllers.score.compute_rsi")
 @patch("app.controllers.score.scoring_nikkei_momentum")
-def test_score_tickerは200レスポンスと正しいbodyを返す(mock_scoring, mock_compute_rsi, mock_get_price_data):
+@patch("app.controllers.score.compute_rsi")
+@patch("app.controllers.score.get_price_data")
+def test_score_tickerは200レスポンスと正しいbodyを返す(
+  mock_get_price_data,
+  mock_compute_rsi,
+  mock_scoring
+):
+  ticker = "7203.T"
+
   mock_get_price_data.return_value = "dummy_df"
   mock_compute_rsi.return_value = 55.123
   mock_scoring.return_value = 37.5
 
-  response = client.get("/score/7203.T")
+  expected = ScoreResponse(
+    ticker = ticker,
+    market_condition = MarketCondition(
+      nikkei_momentum = NikkeiMomentum(
+        score = mock_scoring.return_value,
+        computed_rsi = mock_compute_rsi.return_value
+      )
+    )
+  )
+
+  response = client.get(f"/score/{expected.ticker}")
 
   assert response.status_code == 200
-  data = response.json()
 
-  assert data["ticker"] == "7203.T"
-  assert data["market_condition"]["nikkei_momentum"]["computed_rsi"] == mock_compute_rsi.return_value
-  assert data["market_condition"]["nikkei_momentum"]["score"] == mock_scoring.return_value
+  actual = ScoreResponse(**response.json())
+  assert actual == expected
